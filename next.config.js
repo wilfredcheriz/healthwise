ff2da1152c29216ab98f05917da7c55d92347926
wilfredcheriz <wilfredchez692@gmail.com>
2025-07-21 01:55:24 +0300

    Chore: disable ESLint for Vercel build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true, // ✅ This disables ESLint during builds (e.g., on Vercel)
+  },
 };
 
 module.exports = nextConfig;
