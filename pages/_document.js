c027376ef1d05155cad782cb58be7ef83f068c8c
wilfredcheriz <wilfredchez692@gmail.com>
2025-07-21 03:48:23 +0300

    Add Google Search Console verification tag

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,9 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
             rel="stylesheet"
           />
+          
+          {/* ✅ Google Search Console Verification */}
+         <meta name="google-site-verification" content="W589BpDNLiP0-hHv0xi0clvtRvggLE3FsQw4gE-Cgdc" />
         </Head>
         <body>
           <Main />
